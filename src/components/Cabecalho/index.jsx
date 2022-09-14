7af480c988db89a0bb0b2096a5fd72a7195be67b
nicoles2 <nicole.gomes@estudante.ifms.edu.br>
2022-09-13 21:16:59 -0400

    first commit

---
+++ src/components/Cabecalho/index.jsx
@@ -0,0 +1,8 @@
+export const Cabecalho=()=>{
+    return(
+        <div>
+            <h1>BOCAWEB FRONT</h1>
+            <img src='./src/logobocaweb.png' alt="logo do boca web"/>
+        </div>
+    )
+}
